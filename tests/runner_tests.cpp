ca47dbab13c25db2db6ce6152a51fc3c9f22ad6e
Leonid Moskovets <lmoskovets@alfabank.ru>
2024-11-10 14:01:20 +0400

    add fakeIt stub to template project

--- tests/runner_tests.cpp
+++ tests/runner_tests.cpp
@@ -1,5 +1,26 @@
+#include <fakeit.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include "calculator.h"
+
+using namespace fakeit;
+
+struct SomeInterface {
+    virtual ~SomeInterface(void) = default;
+    virtual int foo(int) = 0;
+    virtual int bar(int, int) = 0;
+    virtual int baz(int*, int&) = 0;
+};
+
+TEST_CASE("Example test", "[example]" ) {
+    Mock<SomeInterface> mock;
+
+    When(Method(mock, foo).Using(Eq(0))).Return(42);
+
+    auto const result = mock.get().foo(0);
+
+    REQUIRE(result == 42);
+    Verify(Method(mock, foo)).Exactly(1);
+}
 
 TEST_CASE("CalculatorTest - Add", "[Calculator]") {
     Calculator calc;
