8e032dd52e1ae476d067dcd2e911e7a6ac282571
Leonid Moskovets <lmoskovets@alfabank.ru>
2024-12-26 14:41:45 +0400

    add new build script, vscode settings, and fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,33 @@
-#!/bin/sh
-BUILD_DIR_NAME=build
-echo "start build..."
+#!/bin/bash
 
-if [[ $PWD == *"scripts"* ]]; then
-  cd .. 
-fi 
+BUILD_DIR_NAME="build"
 
-rm -rf $BUILD_DIR_NAME
-mkdir $BUILD_DIR_NAME
-cmake -S . -B $BUILD_DIR_NAME
+# Check if current called directory is script then change directory to above
+if [ "$(basename "$PWD")" == "scripts" ]; then
+  cd ..
+fi
+
+# Check provided arguments and print them to console else print "No arguments provided"
+if [ $# -eq 0 ]; then
+  echo "No arguments provided"
+else
+  echo "Arguments provided: $@"
+fi
+echo "start building ..."
+
+# Check if arguments contain "test" and perform actions accordingly
+if [[ " $@ " =~ " test " ]]; then
+  rm -rf $BUILD_DIR_NAME
+  mkdir $BUILD_DIR_NAME
+  cd $BUILD_DIR_NAME
+  cmake -S .. -B .
+  make all
+  make test
+  cd ..
+else
+  rm -rf $BUILD_DIR_NAME
+  mkdir $BUILD_DIR_NAME
+  cd $BUILD_DIR_NAME
+  cmake -S .. -B .
+  cd ..
+fi
